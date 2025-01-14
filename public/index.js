dee845343b21b7f16e6d18c99fe7f7719ab9ff88
kokoraro <v.aurora0000@gmail.com>
2025-01-14 11:26:18 +0900

    feat: add colour choices

--- public/index.js
+++ public/index.js
@@ -1,7 +1,19 @@
+import { hexToRgb, rgbToHex } from './utils.js';
+
 const led_server_url = 'http://192.168.86.247:8000';
+let selectedColourBox = null;
+
+let current_colour = [255, 255, 255];
+let off_colour = [0, 0, 0];
 
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
+
+  // Create the grid
+  createGrid(32, 64);
+
+  // Get selected html element and cache it
+  selectedColourBox = document.getElementById('selected-colour-box');
 
   // Add event listener for reset button
   const resetButton = document.getElementById('reset');
@@ -11,13 +23,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const sendButton = document.getElementById('send');
   sendButton.addEventListener('click', onSendClick);
 
+  // Add a listener to each colour button
+  const colourButtons = document.querySelectorAll('.color-button');
+  colourButtons.forEach((button) => {
+    const color = button.getAttribute('data-color');
+    button.style.backgroundColor = color;
+    button.addEventListener('click', onColourClick);
+  });
+
   // Add event listener for mouse move on grid
   const grid = document.getElementById('grid');
   grid.addEventListener('mousemove', onGridMouseMove);
 
-
-  // Create the grid
-  createGrid(32, 64)
 });
 
 function onSendClick() {
@@ -26,11 +43,10 @@ function onSendClick() {
   const pixelData = [];
   cells.forEach((cell) => {
     const position = [cell.getAttribute('data-y'), cell.getAttribute('data-x')]
-    const isActive = cell.classList.contains('active');
-    const rgb = isActive ? [255, 255, 255] : [0, 0, 0];
+    const colour = hexToRgb(cell.getAttribute('data-colour'));
 
     pixelData.push({
-      'rgb': rgb,
+      'rgb': colour,
       'position':position,
     });
   });
@@ -52,6 +68,20 @@ function onSendClick() {
     });
 }
 
+function onColourClick(event) {
+  const colour = event.target.getAttribute('data-color');
+
+  // if it doesn't have a colour attribute, return
+  if (!colour) return;
+
+  // Change the selected colour box
+  selectedColourBox.style.backgroundColor = colour;
+
+  // convert hexcode colour to rgb list
+  const rgb = hexToRgb(colour);
+  current_colour = rgb;
+}
+
 // Create the grid
 function createGrid(rows, columns) {
   const gridElement = document.getElementById('grid');
@@ -68,6 +98,9 @@ function createGrid(rows, columns) {
       // Add data attributes for x and y coordinates
       cell.setAttribute('data-x', i);
       cell.setAttribute('data-y', j);
+
+      // Set current colour
+      setCellColour(cell, off_colour);
 
       // Add event listener for mouse click
       cell.addEventListener('click', onCellClick.bind(null, i, j));
@@ -89,16 +122,24 @@ function onGridMouseMove(event) {
   // check if the target is a cell
   if (!cell.classList.contains('cell')) return;
 
-  cell.classList.add('active');
+  setCellColour(cell, current_colour);
+}
+
+
+function setCellColour(cell, colour) {
+  cell.setAttribute('data-colour', rgbToHex(colour));
+  cell.style.backgroundColor = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
 }
 
 // Handle individual cell click
 function onCellClick(x, y) {
   const cell = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
-  if (cell.classList.contains('active')) {
-    cell.classList.remove('active');
+  const colour = hexToRgb(cell.getAttribute('data-colour'));
+
+  if (colour != off_colour) {
+    setCellColour(cell, off_colour);
   } else {
-    cell.classList.add('active');
+    setCellColour(cell, current_colour);
   }
 }
 
@@ -106,7 +147,7 @@ function onCellClick(x, y) {
 function onResetClick() {
   const cells = document.querySelectorAll('.cell');
   cells.forEach((cell) => {
-    cell.classList.remove('active');
+    setCellColour(cell, off_colour);
   });
 }
 
