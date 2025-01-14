0f7fcd16c4d2ae4c86b7813a931f81d322bb1474
jamesleeksc <jameslee.ksc@gmail.com>
2025-01-14 11:26:18 +0900

    feat: add colour choices

---
+++ public/utils.js
@@ -0,0 +1,22 @@
+
+// Converts a hex code value to a list of RGB values
+function hexToRgb(hex) {
+  // BEGIN: hexToRgb
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return [r, g, b];
+  // END: hexToRgb
+}
+
+// Converts a list of RGB values to their hex code equivalent
+function rgbToHex(rgb) {
+  // BEGIN: rgbToHex
+  const r = rgb[0].toString(16).padStart(2, '0');
+  const g = rgb[1].toString(16).padStart(2, '0');
+  const b = rgb[2].toString(16).padStart(2, '0');
+  return `#${r}${g}${b}`;
+  // END: rgbToHex
+}
+
+export { hexToRgb, rgbToHex };
