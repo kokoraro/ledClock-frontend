84e70653d6473552a28b0fc39d6df1ed1d7b0abb
jamesleeksc <jameslee.ksc@gmail.com>
2025-02-24 18:13:26 +0900

    fix: public directory in docker image

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 // Import necessary packages
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
 
 // Initialize Express app
 const app = express();
@@ -10,22 +10,24 @@ const app = express();
 // Use body-parser middleware to parse JSON requests
 app.use(bodyParser.json());
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-process.on('SIGTERM', process.exit);
-process.on('SIGINT', process.exit);
+process.on("SIGTERM", process.exit);
+process.on("SIGINT", process.exit);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Serve static files from 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/js/utils.js', (req, res) => {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.render('utils', { ledServerURL: process.env.LED_SERVER_URL });
+app.get("/js/utils.js", (req, res) => {
+  res.setHeader("Content-Type", "application/javascript");
+  res.render("utils", { ledServerURL: process.env.LED_SERVER_URL });
 });
 
 // Start the server
